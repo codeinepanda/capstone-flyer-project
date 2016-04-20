4fcc6130c676efc7f085ff7d03c17f2a4209027e
Hayden Thompson <redechox@gmail.com>
2016-04-20 11:26:40 -0400

    added catagory to flyer table

--- database/data.sql
+++ database/data.sql
@@ -12,10 +12,10 @@ INSERT INTO flyer_user (first_name, last_name, password, salt, user_name, email)
 INSERT INTO flyer_user (first_name, last_name, password, salt, user_name, email) VALUES ('Sean', 'Jenkins', 'byXr2Bu18+NoGlGHT3AczA==', 'YnrgCxHq3NS8RQ79qZP6h5vYu+wkyIFqRQ2mCC4zxjHdlAPoh+cJiMECMv7q95pRW8pTbe/LPv8EtVj6W5eUN//YKTICZ1MA6IKkdDlt16x5uyAubpH0CjAOUIJV8WaxYQKze9acfD6pZaQPr6NC7pgYfMWFqODCK+x2c9CcbhI=', 'sjenkins', 'sean@techelevator.com');
 
 -- flyer --
-INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info) VALUES ('TECH ELEVATOR', '1','keving', 'Car Wash', '2016-01-01' , '2016-01-30','4', 'free car wash! redeem tab');
-INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info) VALUES ('TECHNO ESCALATOR', '2','mhaydent', 'FREE DJ', '2016-12-21' , '2016-12-31','5', 'free DJ! redeem tab');
-INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info) VALUES ('TECHNOLOGICAL ELEPHANT', '3','jfunk', 'Mind Reading Class', '2016-11-11' , '2016-12-12','7', 'Come learn how to read your own mind!! redeem tab');
-INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info) VALUES ('LOGICAL ELEPHANT', '4','sjenkins', 'Rebellion Signup', '2016-12-21' , '2016-12-31','20', 'Freedom Fighters Rebellion Sign Up! redeem tab');
+INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info, catagory) VALUES ('TECH ELEVATOR', '1','keving', 'Car Wash', '2016-01-01' , '2016-01-30','4', 'free car wash! redeem tab', 'Automobile');
+INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info, catagory) VALUES ('TECHNO ESCALATOR', '2','mhaydent', 'FREE DJ', '2016-12-21' , '2016-12-31','5', 'free DJ! redeem tab', 'Entertainment');
+INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info, catagory) VALUES ('TECHNOLOGICAL ELEPHANT', '3','jfunk', 'Mind Reading Class', '2016-11-11' , '2016-12-12','7', 'Come learn how to read your own mind!! redeem tab', 'Social');
+INSERT INTO flyer (company, flyer_id, user_name, flyer_name, start_date, end_date, num_tabs, flyer_info, catagory) VALUES ('LOGICAL ELEPHANT', '4','sjenkins', 'Rebellion Signup', '2016-12-21' , '2016-12-31','20', 'Freedom Fighters Rebellion Sign Up! redeem tab', 'Career');
 
 -- tab -- 
 
