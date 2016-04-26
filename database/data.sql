5d611eb627c356cb256b7b1342c5bce537598358
Hayden Thompson <redechox@gmail.com>
2016-04-26 11:40:01 -0400

    added more user with password verygoodpass

--- database/data.sql
+++ database/data.sql
@@ -23,15 +23,15 @@ INSERT INTO flyer_user (first_name, last_name, password, salt, user_name, email)
 
 
 -- flyer --
-INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'TECH ELEVATOR','2', 'kkkkkk', 'Car Wash', '2015-01-01' ,  '2016-01-01' , '2016-11-20','5', '4', 'free car wash! redeem tab');
-INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'TECHNO ESCALATOR', '1','ttttt', 'FREE DJ', '2016-11-12' ,'2016-11-12' , '2016-11-26','5', '5' ,  'free DJ! redeem tab');
-INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'TECHNOLOGICAL ELEPHANT', '3','ffffff', 'Mind Reading Class', '2016-11-11' , '2016-11-11' , '2016-12-12','7', '2' ,'Come learn how to read your own mind!! redeem tab');
-INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'LOGICAL ELEPHANT','4', 'jjjjjj', 'Rebellion Signup', '2016-01-23', '2016-01-23', '2016-10-22','20', '2', 'Freedom Fighters Rebellion Sign Up! redeem tab');
+INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'TECH ELEVATOR','2', 'kev', 'Car Wash', '2015-01-01' ,  '2016-01-01' , '2016-11-20','5', '4', 'free car wash! redeem tab');
+INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'TECHNO ESCALATOR', '1','haydent', 'FREE DJ', '2016-11-12' ,'2016-11-12' , '2016-11-26','5', '5' ,  'free DJ! redeem tab');
+INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'TECHNOLOGICAL ELEPHANT', '3','funky', 'Mind Reading Class', '2016-11-11' , '2016-11-11' , '2016-12-12','7', '2' ,'Come learn how to read your own mind!! redeem tab');
+INSERT INTO flyer (isRetired, company, flyer_id, user_name, flyer_name, create_date, start_date, end_date, num_tabs, tabs_taken, flyer_info) VALUES (false, 'LOGICAL ELEPHANT','4', 'jenkins', 'Rebellion Signup', '2016-01-23', '2016-01-23', '2016-10-22','20', '2', 'Freedom Fighters Rebellion Sign Up! redeem tab');
 
 -- tab -- 
 
-INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (false,'2016-01-01', '1' ,'kkkkkk');
-INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (false,'2016-01-01','2' ,'ttttt');
-INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (true,'2016-01-01', '3' , 'ffffff');
-INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (true,'2016-01-01',  '4' ,'jjjjjj');
+INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (false,'2016-01-01', '1' ,'kev');
+INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (false,'2016-01-01','2' ,'haydent');
+INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (true,'2016-01-01', '3' , 'funky');
+INSERT INTO tab (isRedeemed,pull_date, flyer_id, user_name) VALUES (true,'2016-01-01',  '4' ,'jenkins');
 
